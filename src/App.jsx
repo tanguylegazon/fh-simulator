f775161083cf8c520b0396d64fa01c5fe3119804
Tanguy <98350888+Eseltwift@users.noreply.github.com>
2024-04-17 16:35:27 +0200

    Add multiple phone items

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import StatsPanel from './components/StatsPanel';
 import Phone from './components/Phone';
 
 function App() {
+    const phone_number = 3;
+    const phones = Array(phone_number).fill(<Phone/>);
+
     return (
         <div>
             <Toolbar>
-                <Phone/>
+                {phones}
+                <div style={{border: '3px dashed red', borderRadius: '50%', width: '44px', height: '44px'}}></div>
             </Toolbar>
             <Antenna/>
             <StatsPanel/>
