956c6939f76c5dc9fc2c231e707f081211dc8cea
Corentin DURUPT <coco.durupt@gmail.com>
2024-04-19 12:57:05 +0200

    add hooping graph

---
+++ src/components/Graph.jsx
@@ -0,0 +1,64 @@
+import './Graph.css';
+
+function Graph(){
+    return(
+        <div className='div_graph'>
+            <table className='table_graph'>
+                <thead>
+                    <tr>
+                        <th className='th_graph'></th>
+                        <th className='th_graph'>T1</th>
+                        <th className='th_graph'>T2</th>
+                        <th className='th_graph'>T3</th>
+                        <th className='th_graph'>T4</th>
+                        <th className='th_graph'>T5</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='td_graph'></td>
+                        <td className='td_graph'>F1</td>
+                        <td className='td_graph'>F2</td>
+                        <td className='td_graph'>F3</td>
+                        <td className='td_graph'>F4</td>
+                        <td className='td_graph'>F5</td>
+                    </tr>
+                    <tr>
+                        <td className='td_graph'></td>
+                        <td className='td_graph'>F1</td>
+                        <td className='td_graph'>F2</td>
+                        <td className='td_graph'>F3</td>
+                        <td className='td_graph'>F4</td>
+                        <td className='td_graph'>F5</td>
+                    </tr>
+                    <tr>
+                        <td className='td_graph'></td>
+                        <td className='td_graph'>F1</td>
+                        <td className='td_graph'>F2</td>
+                        <td className='td_graph'>F3</td>
+                        <td className='td_graph'>F4</td>
+                        <td className='td_graph'>F5</td>
+                    </tr>
+                    <tr>
+                        <td className='td_graph'></td>
+                        <td className='td_graph'>F1</td>
+                        <td className='td_graph'>F2</td>
+                        <td className='td_graph'>F3</td>
+                        <td className='td_graph'>F4</td>
+                        <td className='td_graph'>F5</td>
+                    </tr>
+                    <tr>
+                        <td className='td_graph'></td>
+                        <td className='td_graph'>F1</td>
+                        <td className='td_graph'>F2</td>
+                        <td className='td_graph'>F3</td>
+                        <td className='td_graph'>F4</td>
+                        <td className='td_graph'>F5</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Graph;
