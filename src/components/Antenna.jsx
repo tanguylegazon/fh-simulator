5eeb2690b0ac9b427f7608a7969dcb1328676103
Tanguy <eseltwift@gmail.com>
2024-06-02 16:19:31 +0200

    Change antenna image

--- src/components/Antenna.jsx
+++ src/components/Antenna.jsx
@@ -2,8 +2,8 @@ import './Antenna.css';
 
 function Antenna() {
     return (
-        <div className="div_antenna">
-            <img className="img_antenna" src="/src/pictures/antenna.png" alt="antenna" />
+        <div className='antenna-area'>
+            <div className='antenna'></div>
             <div className='radar'></div>
         </div>
     );
